af851413a995eb4b5d442f6c8e0604b27bcef249
K-Peggy <peggy110336@gmail.com>
2015-12-13 15:45:17 +0800

    compelete AB_Game

--- AB_Game/ViewController.swift
+++ AB_Game/ViewController.swift
@@ -9,10 +9,115 @@
 import UIKit
 
 class ViewController: UIViewController {
+    let error_mag = "請輸入四個不重複的數字"
+    var ans: [Int] = [1,6,7,2]
+
+    @IBOutlet weak var tf_input: UITextField!
+    
+    @IBOutlet weak var label_result: UILabel!
+    
+    @IBAction func btn_guess(sender: UIButton) {
+        let input: String? = tf_input.text!
+        
+        if input == nil {
+            label_result.text = error_mag
+            return
+        }
+        var num_array: [Int] = [0,0,0,0]
+        if CheckNum(input!,num_array: &num_array) == false{
+            return
+        }
+        for var i = 0; i < 4; i++ {
+            print("\(num_array[i])")
+        }
+        
+        
+        let result: String = getresult(num_array)
+        label_result.text = result
+        
+    }
+    
+    func generateAns(){
+        for var i=0; i<4; i++ {
+            var flag = false
+            repeat {
+                let randomnum = random()%10
+                for var j=0; j<i;j++ {
+                    if ans[j] == randomnum {
+                        flag == true
+                        break;
+                    }
+                }
+            if flag == false {
+                ans[i] = randomnum
+            }
+            }while(flag == true)
+        }
+    }
+    
+    func getresult(num_array: [Int])->String{
+        var a: Int = 0
+        var b: Int = 0
+        
+        for var i=0; i<4; i++ {
+            for var j=0;j<4;j++ {
+                if i==j {
+                    if num_array[i] == ans[j]{
+                        a++
+                    }
+                    else{
+                        if num_array[i] == ans[j]{
+                            b++
+                        }
+                    }
+                }
+            }
+        }
+        let result = "\(a)A\(b)B"
+        return result
+    }
+    
+    func CheckNum(input: String,inout num_array:[Int]) -> Bool{
+        if input.characters.count != 4 {
+            label_result.text = "長度必須為4"
+            return false
+        }
+        let number: Int? = Int(input)
+        if number == nil {
+            label_result.text = error_mag
+            return false
+        }
+        
+        num_array[0] = number! / 1000
+        num_array[1] = (number! % 1000) / 100
+        num_array[2] = ((number! % 1000) % 100) / 10
+        num_array[3] = ((number! % 1000) % 100) % 10
+        
+        var flag = false
+        for var i=0; i<4; i++ {
+            for var j=0; j<4; j++ {
+                if i == j{
+                    continue
+                }
+                if num_array[i] == num_array[j]{
+                    flag = true
+                    break
+                }
+            }
+        }
+        if flag == true {
+            return false
+        }
+        return true
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        generateAns()
+        for var i=0; i<4; i++ {
+            print("\(i): \(ans[i])")
+        }
     }
 
     override func didReceiveMemoryWarning() {
